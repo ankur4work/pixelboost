a5e14ec30f640e9595e745927fe0bce07a31102d
ankursing4u <a4ankur.mail@gmail.com>
2026-05-30 14:14:12 +0530

    Fix 500: remove billing gate from layout loader

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -2,20 +2,14 @@ import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider as ShopifyAppProvider } from "@shopify/shopify-app-react-router/react";
 import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
-import { authenticate, PLAN_BASIC } from "../shopify.server";
+import { authenticate } from "../shopify.server";
 
 import "@shopify/polaris/build/esm/styles.css";
 
 import enTranslations from "@shopify/polaris/locales/en.json";
 
 export const loader = async ({ request }) => {
-  const { billing } = await authenticate.admin(request);
-
-  const billingCheck = await billing.require({
-    plans: [PLAN_BASIC],
-    isTest: true,
-    onFailure: () => billing.request({ plan: PLAN_BASIC, isTest: true }),
-  });
+  await authenticate.admin(request);
 
   // eslint-disable-next-line no-undef
   return { apiKey: process.env.SHOPIFY_API_KEY || "" };
